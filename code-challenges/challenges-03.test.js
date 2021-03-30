3935d30a7e6c90eb6b00ca64996ab727197867f1
arnone215 <tarnone21589@gmail.com>
2021-03-29 22:40:45 -0700

    question 3 completed

--- code-challenges/challenges-03.test.js
+++ code-challenges/challenges-03.test.js
@@ -38,6 +38,8 @@ For example, containsAnd(['panda', 'ran', 'and']) returns ['panda', 'and'].
 
 const containsAnd = (arr) => {
   // Solution code here...
+  let answer = arr.filter(each => each.includes('and'));
+  return answer;
 };
 
 /* ------------------------------------------------------------------------------------------------
