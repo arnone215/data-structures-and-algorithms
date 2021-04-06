ee4cfc0a4ffbe0f6ebf32f32546b1dc10a540e0b
arnone215 <tarnone21589@gmail.com>
2021-04-06 00:25:14 -0700

    map questions pushed

--- code-challenges/challenges-02.test.js
+++ code-challenges/challenges-02.test.js
@@ -25,6 +25,7 @@ Write a function named addOne that, given an array of numbers, uses map to retur
 
 const addOne = (arr) => {
   // Solution code here...
+  // solution code here...
   const answer = [];
   arr.forEach((value) => {
     answer.push(value + 1);
